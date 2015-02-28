7bdb722369dc619c7d891104f56da1f9bcd5b544
mrjadaml <mrjadaml@gmail.com>
2015-02-28 16:52:48 -0700

    skipping verify_authenticity_token to allow POST request

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -1,7 +1,13 @@
 class RantsController<ApplicationController
+  skip_before_filter :verify_authenticity_token
 
   def index
     render json: Rant.all
+  end
+
+  def create
+    rant = Rant.create(params.require(:rant).permit(:title, :body))
+    render json: rant
   end
 
 end
