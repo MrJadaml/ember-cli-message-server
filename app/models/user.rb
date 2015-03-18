e2ab49664fb627a01aba05a1bd21743727a28c12
mrjadaml <mrjadaml@gmail.com>
2015-03-18 15:01:05 -0600

    removed protect_from_forgery in app controller. set undefined user for find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.find_by_credentials(email, password)
-    user.authenticate(password) if User.find_by(email: email)
+    if User.find_by(email: email)
+      user = User.find_by(email: email)
+      user.authenticate(password)
+    end
   end
 
   def generate_auth_token
