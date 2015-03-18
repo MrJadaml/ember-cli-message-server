6a76e5c0e36463df5fab1ac39036b8f79275002c
mrjadaml <mrjadaml@gmail.com>
2015-03-18 11:32:11 -0600

    added user model w/ auth token methods, auth token library, login route

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+require_relative "../../lib/auth/auth_token"
+
+class User < ActiveRecord::Base
+  has_secure_password
+
+  def self.find_by_credentials(email, password)
+    user.authenticate(password) if User.find_by(email: email)
+  end
+
+  def generate_auth_token
+    payload = { user_id: self.id }
+    AuthToken.encode(payload)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   resources :rants
-
+  post 'login' => 'auth#authenticate'
 end
