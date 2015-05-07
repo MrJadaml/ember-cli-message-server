385eb8ee279216241f0930f165727edc39779a86
mrjadaml <mrjadaml@gmail.com>
2015-05-07 16:15:45 -0700

    added index to user controller and name attrs to user seralizer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   skip_before_filter :authenticate_request
 
+  def index
+    render json: User.all
+  end
+  
   def show
     render json: User.find(params[:id])
   end
