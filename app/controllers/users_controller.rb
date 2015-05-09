7713da63500323150fb2dec7e57bb5ff78479a2b
mrjadaml <mrjadaml@gmail.com>
2015-05-09 11:23:35 -0700

    removed unnecessary private params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,4 @@ class UsersController < ApplicationController
     end
   end
 
-private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
-
 end
