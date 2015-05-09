f3ad03c9df3d951047f4cf75edf413435ae074e0
mrjadaml <mrjadaml@gmail.com>
2015-05-08 17:02:13 -0700

    user#create pulling in params data and creating new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,26 @@ class UsersController < ApplicationController
   def index
     render json: User.all
   end
-  
+
   def show
     render json: User.find(params[:id])
+  end
+
+  def create
+    user = User.new(first_name: params['firstName'],
+                    last_name: params['lastName'],
+                    email: params['email'],
+                    password: params['password'],
+                    password_confirmation: params['passwordConfirmation'])
+    if user.save
+      render json: user, meta: { success: true, auth_token: user.generate_auth_token, user_id: user.id }
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   resources :rants, :users
   post 'login' => 'auth#authenticate'
+  post 'signup' => 'users#create'
 end
